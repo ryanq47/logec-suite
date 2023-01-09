07739af154b0a5374d126ca7f5b570b00365ee0a
kali <kali@kali.kali>
2023-01-09 13:32:17 -0600

    Added start of installer bash file

---
+++ installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "####################"
+echo "Logec Installer!"
+echo "####################"
+
+echo "##=================="
+echo "Installing PipReqs..."
+echo "##=================="
+
+pip install -r requirements.txt
+
