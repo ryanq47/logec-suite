cf7efdc62d42ee7c6473d5590f53c9b2fe69763e
ryanq47 <ryankleffman47@gmail.com>
2023-02-28 11:47:17 -0600

    started backend for project files

---
+++ Modules/General/SaveFiles/fileops.py
@@ -0,0 +1,36 @@
+from zipfile import ZipFile
+
+from datetime import datetime, timezone
+import os, sys
+from shutil import make_archive, unpack_archive
+
+class SaveFiles:
+    def __init__(self):
+        pass
+        
+    
+    def save_framework(self, options):
+        action, self.filename = options
+        
+        self.sys_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+        
+        
+        self.time = datetime.now(timezone.utc)
+        
+        
+    def createfile(self):
+        os.mkdir(f'{self.sys_path}/Modules/General/SaveFiles/Projects')
+        os.mkdir(f'{self.sys_path}/Modules/General/SaveFiles/.tmp_projectfolder')
+    
+    def savefile(self):
+        make_archive(f'{self.sys_path}/Modules/General/SaveFiles/Projects/{self.filename}','zip',f'{self.sys_path}/Modules/General/SaveFiles/.tmp_projectfolder/')
+
+    def loadfile(self):
+        unpack_archive(f'{self.sys_path}/Modules/General/SaveFiles/Projects/{self.filename}',f'{self.sys_path}/Modules/General/SaveFiles/.tmp_projectfolder/','zip')
+
+S = SaveFiles()
+
+S.save_framework([1,2])
+S.savefile()
+#S.loadfile()
+
